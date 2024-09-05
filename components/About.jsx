a819226be9e98bbcb91e435e5eab8c76f99eca9a
irfiacre <irfiacre@gmail.com>
2024-09-05 23:14:40 +0200

    Improve about me

--- components/About.jsx
+++ components/About.jsx
@@ -1,38 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
-import avatar4 from "../public/assets/avatar4.jpeg";
+import profile from "../public/assets/img/me/profile2.png";
 
 function About() {
   return (
     <div id="about" className="w-full lg:h-screen py-20 flex items-center">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 px-6">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#5651e5]">Acerca de mi</p>
-          <h2 className="py-4">Quién soy?</h2>
-          <p className="py-2 text-gray-600">
-            Soy un Desarrollador de Software de Argentina con más de 1 año de experiencia
-            en el desarrollo de soluciones que mejoran la experiencia del usuario. Tengo
-            experiencia en entornos dinámicos y de ritmo rápido, incluyendo una startup
-            sueca y trabajos freelance. Con un gran ojo para los detalles y una fuerte
-            mentalidad de resolución de problemas, estoy dedicado a entregar soluciones de
-            alta calidad.
-          </p>
-          <p className="py-2 text-gray-600">
-            He utilizado diferentes lenguajes y herramientas para construir aplicaciones
-            web como Php, Laravel, JavaScript, TypeScript, Node JS, Express JS, React JS,
-            Tailwind, Material-UI, pruebas unitarias, Firebase y Amazon S3. Lo que más
-            disfruto es aprender y usar tecnologías para construir aplicaciones tanto de
-            frontend como de backend.
+          <p className="uppercase text-xl tracking-widest text-primary">About me!</p>
+          <h2 className="py-4">Who am I?</h2>
+          <p className="py-2 text-gray-600 font-light">
+            I am a Software Developer from Rwanda with over <strong className="text-base">4</strong> year of professional experience 
+            developing solutions that improve user experience. I have experience in 
+            fast-paced and dynamic environments, including a German International company.
+            <br /> 
+            With a great eye for detail and a strong problem-solving mindset, I am dedicated to 
+            delivering high-quality solutions.
           </p>
 
           <Link legacyBehavior href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
-              Mirá mis últimos proyectos.
+              Get to know my work.
             </p>
           </Link>
         </div>
         <div className="max-w-xs mx-auto mt-4 h-fit self-center shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center hover:scale-105 ease-in duration-150">
-          <Image src={avatar4} className="rounded-xl " alt="/" />
+          <Image src={profile} className="rounded-xl object-cover" alt="/" />
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+"use client";
 import Head from "next/head";
+import { Poppins } from "next/font/google";
 import Main from "../components/Main";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function Home() {
   return (
@@ -19,10 +26,12 @@ export default function Home() {
           style={{ color: "white" }}
         />
       </Head>
-      <Main />
-      <About />
-      <Skills />
-      <Projects />
+      <div className={poppins.className}>
+        <Main />
+        <About />
+        <Skills />
+        <Projects />
+      </div>
     </div>
   );
 }
