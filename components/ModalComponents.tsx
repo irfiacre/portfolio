cb1e97fadea2756fb4ccd906704021a17c290798
irfiacre <irfiacre@gmail.com>
2024-09-10 23:48:17 +0200

    Improving moodel

--- components/ModalComponents.tsx
+++ components/ModalComponents.tsx
@@ -13,7 +13,7 @@ const ModalComponent = () => {
           <div className="relative bg-white rounded-lg">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
               <h3 className="text-xl font-semibold text-gray-900 ">
-                Terms of Service
+                Project Name
               </h3>
               <button
                 type="button"
@@ -37,21 +37,6 @@ const ModalComponent = () => {
                 </svg>
                 <span className="sr-only">Close modal</span>
               </button>
-            </div>
-
-            <div className="p-4 md:p-5 space-y-4">
-              <p className="text-base leading-relaxed">
-                With less than a month to go before the European Union enacts
-                new consumer privacy laws for its citizens, companies around the
-                world are updating their terms of service agreements to comply.
-              </p>
-              <p className="text-base leading-relaxed">
-                The European Union's General Data Protection Regulation
-                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
-                common set of data rights in the European Union. It requires
-                organizations to notify users as soon as possible of high-risk
-                data breaches that could personally affect them.
-              </p>
             </div>
 
             <div className="flex items-center p-4 md:p-5 border-t">
