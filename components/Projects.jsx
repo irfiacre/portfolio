cb78818f7a4769d02d5c9e36d8ecfa7377188a14
irfiacre <irfiacre@gmail.com>
2024-09-19 22:37:38 +0200

    Improvements

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,11 +2,18 @@ import ProjectItem from "./ProjectItem";
 import { projects } from "./../data/projects";
 
 function Projects() {
+
   return (
     <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-6 py-16">
         <p className="text-xl tracking-widest uppercase text-primary">
-          Personal Projects
+          Personal Projects (To USE a GITHUB API, For this)
+        </p>
+        <p>
+          - Repos: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28
+          - Auth: https://docs.github.com/en/rest/using-the-rest-api/getting-started-with-the-rest-api?apiVersion=2022-11-28#authentication
+          - Commits: https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28
+          
         </p>
         <br />
         <div className="grid md:grid-cols-2 gap-8">
