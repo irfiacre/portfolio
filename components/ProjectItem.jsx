3a6fc21da0e149aab77ab301a67ca50a5689aa15
irfiacre <irfiacre@gmail.com>
2024-10-09 18:13:38 +0200

    Improve initial projects view

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -7,19 +7,25 @@ import BaseCard from "./base/BaseCard";
 
 const ProjectItem = ({ details }) => {
   const [open, setOpen] = useState(false);
-
+  
   return (
-    <div className="relative flex items-center justify-center h-auto w-full rounded-md group py-1">
+    <div className="cursor-pointer relative flex items-center justify-center h-auto w-full rounded-md group py-1">
       {open && (
-        <BaseModel
-          projectContent={details}
-          onClose={() => setOpen(false)}
-        />
+        <BaseModel projectContent={details} onClose={() => setOpen(false)} />
       )}
-      
-      <BaseCard className="p-4" onClick={()=>setOpen(true)}>
-        <h3 className="py-2 capitalize cursor-pointer">{details.name}</h3>
-        <p>{`${details.description?.substring(0, 50)}...`}</p>
+      <BaseCard className="p-4" onClick={() => setOpen(true)}>
+        <h3 className="py-2 capitalize">{details.name}</h3>
+        <p>{details.description?.length >50?`${details.description?.substring(0, 50)}...`:details.description}</p>
+        <div className="w-full py-2 flex flex-wrap items-center justify-start">
+          {[details.language, ...details.topics].map((topic) => (
+            topic && <span
+              key={topic}
+              className="rounded-full bg-primary/10 p-2 hover:scale-105 ease-in duration-300 text-gray-500 text-sm m-1"
+            >
+              {topic}
+            </span>
+          ))}
+        </div>
       </BaseCard>
     </div>
   );
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,34 +5,49 @@ import handleGetGithubProjects from "../utils/projects";
 
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [paginatedProjects, setPaginatedProjects] = useState([]);
+
   useEffect(() => {
     (async () => {
       const response = await handleGetGithubProjects();
       setProjects(response);
+      setPaginatedProjects(response);
     })();
   }, []);
+
+  console.log(paginatedProjects);
+  const handleLoadMore = () => {
+    console.log("-------> Should update Paginated projects");
+  };
 
   return (
     <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-6 py-16">
         <p className="text-xl tracking-widest uppercase text-primary">
-          Personal Projects <span className="text-xs capitalize">(last GitHub Sync just now)</span>
+          Projects
         </p>
-        <div className="grid md:grid-cols-2 gap-4">
-          {projects.map((project) => (
-            <ProjectItem
-              details={project}
-              key={project.name}
-            />
-          ))}
-          {/* <div style={{ height: '100vh', width: '100%' }}>
-      <iframe
-        src="http://driverhub.netlify.app/"
-        title="Driver Hub"
-        style={{ width: '100%', height: '100%', border: 'none' }}
-        allowFullScreen
-      />
-    </div> */}
+        <div>
+          <h2 className="py-4">GitHub Projects</h2>
+          <span className="text-xs capitalize text-primary/50">
+            last Sync (Just now)
+          </span>
+          <div className="grid md:grid-cols-2 gap-4">
+            {projects.map((project) => (
+              <ProjectItem details={project} key={project.name} />
+            ))}
+          </div>
+          <div className="flex items-center mt-4 cursor-pointer">
+            <div className="border-t border border-primary/20 flex-grow"></div>
+            <div
+              onClick={handleLoadMore}
+              className="px-3 text-primary font-light hover:font-medium text-sm p-2 rounded-full border border-primary/20 hover:bg-primary/20"
+            >
+              {paginatedProjects.lenght === projects.lenght
+                ? "End"
+                : "Load More"}
+            </div>
+            <div className="border-t border border-primary/20 flex-grow"></div>
+          </div>
         </div>
       </div>
     </div>
